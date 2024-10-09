a844978723afde7ef4a894c17bd5db973c382423
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-10-09 15:08:30 -0600

    Agregar archivos de proyecto.

---
+++ FJCO.TemperaturaTests/TemperatureConverterTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FJCO.Temperatura;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FJCO.Temperatura.Tests
+{
+    [TestClass()]
+    public class TemperatureConverterTests
+    {
+        [TestMethod()]
+        public void CelsiusToFahrenheitTest()
+        {
+            TemperatureConverter temperatureConverter = new TemperatureConverter();
+            double celsius = 0;
+            double expected = 32;
+            double actual = temperatureConverter.CelsiusToFahrenheit(celsius);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FahrenheitToCelsiusTest()
+        {
+            TemperatureConverter temperatureConverter = new TemperatureConverter();
+            double fahrenheit = 32;
+            double expected = 0;
+            double actual = temperatureConverter.FahrenheitToCelsius(fahrenheit);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
